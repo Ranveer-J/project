fd56912b8d8a813c04b6dd6be9715a7c5ef72fce
Ranveer-J <83578974+Ranveer-J@users.noreply.github.com>
2024-11-25 17:29:13 +0000

    Add files via upload

--- P!.py
+++ P!.py
@@ -8,12 +8,10 @@ gpa = input("Enter your GPA: ")
 attendance = input("Enter your attendance percentage: ")
 
 if int(age) < 18:
-        print("Sorry,",name, "you are not eligible because you are below 18 years old.")
-   elif float(gpa) < 2.5:
-        print("Sorry, " ,name " you are not eligible because your GPA is below 2.5.")
-   elif int(attendance) < 75:
-        print("Sorry, " ,name ", you are not eligible because your attendance is below 75%.")
-   else:
-        print("Congratulations, " ,name "! You are eligible for the exam.")
+    print("sorry, you are not elidgeble for this exam because you are under 18")
+elif float(gpa) < 2.5:
+    print("Sorry, you are not elidgeble for this exam because your GPA is too low")
+elif int(attendance) < 70:
+    print("Sorry, you are not elidgeble for this exam because your attendance is too low")
 
 
